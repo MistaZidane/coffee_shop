2cb1e3e65959de9f027e63abef6b40d530a0882c
mistazidane <ntokungwiazidane@yahoo.com>
2022-12-09 00:42:33 +0100

    basic nav structure

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,8 +3,33 @@
 </script>
 
 <template>
-   <nav>
+   <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-      </nav>
+      </nav> -->
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <RouterLink to="/" class="navbar-brand" >Coffee</RouterLink>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <RouterLink to="/about" class="nav-link">Cafe Menu</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/about" class="nav-link">About Us</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/about" class="nav-link">Find Us</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/about" class="nav-link">Products</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 </template>
