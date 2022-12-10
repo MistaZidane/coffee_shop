1195b0db2de61b594aebe7502816cbab93e9b5fb
mistazidane <ntokungwiazidane@yahoo.com>
2022-12-10 10:33:29 +0100

    added routing animations

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,30 @@ import Navbar from './components/Navbar.vue'
 
 <template>
   <Navbar/>
-  <router-view v-slot="{ Component, route }">
-  <!-- Use any custom transition and  to `fade` -->
-  <transition :name="route.meta.transitionName || 'fade'">
-    <component :is="Component" />
-  </transition>
-</router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="slide" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
   
  
 </template>
 <style scoped>
+.slide-enter-active {
+  transition: all 0.3s ease-out;
+}
 
+.slide-enter-from {
+  /* transform: translateX(100px); */
+  z-index: 0;
+  opacity: 0;
+}
+.slide-leave-to{
+opacity: 0;
+/* transform: translateX(-100px); */
+z-index: 10;
+} 
+.slide-leave-active{
+  transition: all 0.3s ease-in;
+}
 </style>
