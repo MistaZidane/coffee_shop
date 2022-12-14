9ca1cebb7f5faeb52f8cbde9d16abd5b711e6a0c
mistazidane <ntokungwiazidane@yahoo.com>
2022-12-14 19:11:43 +0100

    added resposiveness to main page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,15 +5,16 @@
 <template>
     <div class="main">
          <div class="container"> 
-  <div class="row flex-lg-row-reverse align-items-center  ">
-    <div class="col-md-6">
-      <img src="../assets/header.jpg" alt="" class="img-fluid my-img">
-    </div>
-    <div class="col-md-6">
-      <h1 class="intro fw-bold lh-1 mb-3">Coffreka Delicious Coffee</h1>
+  <div class="row  align-items-center  ">
+    <div class="col-md-6 col-sm-12">
+      <h1 class="intro fw-bold lh-1 mb-3" id="intro">Coffreka Delicious Coffee</h1>
       <p class="lead">We are coffee punks who don’t exaggerate about their Americanos. Time, temperature, and technique need to be on point to make the best cup of coffee, but it’s all for nothing without quality beans roasted to perfection.  </p>
       <RouterLink to="/menu" class="btn my-btn btn-lg">Shop Coffee</RouterLink>
     </div>
+    <div class="col-md-6">
+      <img src="../assets/header.jpg" alt="" class="img-fluid my-img header-img">
+    </div>
+ 
   </div>
   </div>
     </div>
